bb95ff6caeb986318300fbfecb4c0ab8eb354da1
sruj <chryplewiczpawel@wp.pl>
2018-01-19 17:17:35 +0100

    cleaning

--- src/AppBundle/Tests/Controller/API/ProgrammerControllerTest.php
+++ src/AppBundle/Tests/Controller/API/ProgrammerControllerTest.php
@@ -21,29 +21,13 @@ class ProgrammerControllerTest extends ApiTestCase
 
     public function testPOST()
     {
-
-        $debuggingQuerystring = '';
-        if (isset($_GET['XDEBUG_SESSION_START'])) { // xdebug
-            $debuggingQuerystring = 'XDEBUG_SESSION_START=' . $_GET['XDEBUG_SESSION_START'];
-        }
-        if (isset($_COOKIE['XDEBUG_SESSION'])) { // xdebug (cookie)
-            $debuggingQuerystring = 'XDEBUG_SESSION_START=PHPSTORM';
-        }
-        if (isset($_GET['start_debug'])) { // zend debugger
-            $debuggingQuerystring = 'start_debug=' . $_GET['start_debug'];
-        }
-        if (empty($debuggingQuerystring)) {
-            $debuggingQuerystring = 'XDEBUG_SESSION_START=PHPSTORM';
-        }
-        $debuggingQuerystring = '';
-
         $data = array(
             'nickname' => 'ObjectOrienter',
             'avatarNumber' => 5,
             'tagLine' => 'a test dev!'
         );
 
-        $response = $this->client->post('/knp_Symfony_RESTful_API_Trenning_2018/web/app_dev.php/api/programmers' . '?' . $debuggingQuerystring, [
+        $response = $this->client->post('/knp_Symfony_RESTful_API_Trenning_2018/web/app_dev.php/api/programmers', [
             'body' => json_encode($data)
         ]);
         $header = $response->getHeaders();
