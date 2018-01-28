f4c2d0beaa1ad73aa3077f0b45411cb6ce531c9a
sruj <chryplewiczpawel@wp.pl>
2018-01-28 12:12:24 +0100

    Endpoints: POST, GET, PUT, PATCH, DELETE.

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -2,15 +2,19 @@
 
 namespace AppBundle\Controller\Api;
 
+use AppBundle\Api\ApiProblem;
+use AppBundle\Api\ApiProblemException;
 use AppBundle\Controller\BaseController;
 use AppBundle\Entity\Programmer;
 use AppBundle\Form\ProgrammerType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Form\UpdateProgrammerType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ProgrammerController extends BaseController
 {
@@ -20,21 +24,25 @@ class ProgrammerController extends BaseController
      */
     public function newAction(Request $request)
     {
-        $data = json_decode($request->getContent(), true);
         $programmer = new Programmer();
         $form = $this->createForm(new ProgrammerType(), $programmer);
-        $form->submit($data);
+        $this->processForm($request, $form);
+
+        if (!$form->isValid()) {
+            $this->throwApiProblemValidationException($form);
+        }
+
         $programmer->setUser($this->findUserByUsername('weaverryan'));
+
         $em = $this->getDoctrine()->getManager();
         $em->persist($programmer);
         $em->flush();
 
-        $data = $this->serializeProgrammer($programmer);
+        $response = $this->createApiResponse($programmer, 201);
         $programmerUrl = $this->generateUrl(
             'api_programmers_show',
             ['nickname' => $programmer->getNickname()]
         );
-        $response = new JsonResponse($data, 201);
         $response->headers->set('Location', $programmerUrl);
 
         return $response;
@@ -42,6 +50,7 @@ class ProgrammerController extends BaseController
 
     /**
      * @Route("/api/programmers/{nickname}", name="api_programmers_show")
+     * @Method("GET")
      */
     public function showAction($nickname)
     {
@@ -49,14 +58,20 @@ class ProgrammerController extends BaseController
             ->getRepository('AppBundle:Programmer')
             ->findOneByNickname($nickname);
 
-        $data = $this->serializeProgrammer($programmer);
-        $response = new JsonResponse($data, 200);
+        if (!$programmer) {
+            throw $this->createNotFoundException(sprintf(
+                'No programmer found with nickname "%s"',
+                $nickname
+            ));
+        }
+
+        $response = $this->createApiResponse($programmer, 200);
 
         return $response;
     }
 
     /**
-     * @Route("/api/programmers/", name="api_programmers_list")
+     * @Route("/api/programmers")
      * @Method("GET")
      */
     public function listAction()
@@ -65,24 +80,107 @@ class ProgrammerController extends BaseController
             ->getRepository('AppBundle:Programmer')
             ->findAll();
 
-        $data = ['programmers' => []];
-
-        foreach ($programmers as $programmer) {
-            $data['programmers'][] = $this->serializeProgrammer($programmer);
-        }
-
-        $response = new JsonResponse($data, 200);
+        $response = $this->createApiResponse(['programmers' => $programmers], 200);
 
         return $response;
     }
 
-    private function serializeProgrammer(Programmer $programmer)
+    /**
+     * @Route("/api/programmers/{nickname}")
+     * @Method({"PUT", "PATCH"})
+     */
+    public function updateAction($nickname, Request $request)
     {
-        return array(
-            'nickname' => $programmer->getNickname(),
-            'avatarNumber' => $programmer->getAvatarNumber(),
-            'powerLevel' => $programmer->getPowerLevel(),
-            'tagLine' => $programmer->getTagLine(),
+        $programmer = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findOneByNickname($nickname);
+
+        if (!$programmer) {
+            throw $this->createNotFoundException(sprintf(
+                'No programmer found with nickname "%s"',
+                $nickname
+            ));
+        }
+
+        $form = $this->createForm(new UpdateProgrammerType(), $programmer);
+        $this->processForm($request, $form);
+
+        if (!$form->isValid()) {
+            $this->throwApiProblemValidationException($form);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        $response = $this->createApiResponse($programmer, 200);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/programmers/{nickname}")
+     * @Method("DELETE")
+     */
+    public function deleteAction($nickname)
+    {
+        $programmer = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findOneByNickname($nickname);
+
+        if ($programmer) {
+            // debated point: should we 404 on an unknown nickname?
+            // or should we just return a nice 204 in all cases?
+            // we're doing the latter
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($programmer);
+            $em->flush();
+        }
+
+        return new Response(null, 204);
+    }
+
+    private function processForm(Request $request, FormInterface $form)
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            $apiProblem = new ApiProblem(400, ApiProblem::TYPE_INVALID_REQUEST_BODY_FORMAT);
+
+            throw new ApiProblemException($apiProblem);
+        }
+
+        $clearMissing = $request->getMethod() != 'PATCH';
+        $form->submit($data, $clearMissing);
+    }
+
+    private function getErrorsFromForm(FormInterface $form)
+    {
+        $errors = array();
+        foreach ($form->getErrors() as $error) {
+            $errors[] = $error->getMessage();
+        }
+
+        foreach ($form->all() as $childForm) {
+            if ($childForm instanceof FormInterface) {
+                if ($childErrors = $this->getErrorsFromForm($childForm)) {
+                    $errors[$childForm->getName()] = $childErrors;
+                }
+            }
+        }
+
+        return $errors;
+    }
+
+    private function throwApiProblemValidationException(FormInterface $form)
+    {
+        $errors = $this->getErrorsFromForm($form);
+
+        $apiProblem = new ApiProblem(
+            400,
+            ApiProblem::TYPE_VALIDATION_ERROR
         );
+        $apiProblem->set('errors', $errors);
+
+        throw new ApiProblemException($apiProblem);
     }
 }
