92005e0f9d428cecd5f49b0ff7e9605b7e49e949
sruj <chryplewiczpawel@wp.pl>
2018-01-28 13:23:01 +0100

    Exception. Klasa exception i klasa pomocnicza.

--- testing.php
+++ testing.php
@@ -38,12 +38,12 @@ $response = $client->post('/knp_Symfony_RESTful_API_Trenning_2018/web/app_dev.ph
     'body' => json_encode($data)
 ]);
 
-$programmerUrl = $response->getHeader('Location');
-// 2) GET a programmer resource
-$response = $client->get($programmerUrl);
-
-// 3) GET a programmers collection resource
-$response = $client->get('/knp_Symfony_RESTful_API_Trenning_2018/web/app_dev.php/api/programmers/' . '?' . $debuggingQuerystring);
+//$programmerUrl = $response->getHeader('Location');
+//// 2) GET a programmer resource
+//$response = $client->get($programmerUrl);
+//
+//// 3) GET a programmers collection resource
+//$response = $client->get('/knp_Symfony_RESTful_API_Trenning_2018/web/app_dev.php/api/programmers/' . '?' . $debuggingQuerystring);
 
 echo $response;
 echo "\n\n";
